1305b27da344580ced5bf9997120c5a86e069bfe
Hưng Nguyễn Xuân <19521578@ms.uit.edu.vn>
2021-06-03 14:32:19 +0700

    Initialization App

---
+++ android/app/src/main/kotlin/com/example/hk_clothes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hk_clothes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MainScreens extends StatefulWidget {
+  MainScreens({Key key}) : super(key: key);
+
+  @override
+  _MainScreensState createState() => _MainScreensState();
+}
+
+class _MainScreensState extends State<MainScreens> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
