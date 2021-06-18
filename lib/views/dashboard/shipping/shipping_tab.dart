66daaa15729575353e5e809189e95e38aeb8d9a0
Mikaaa <kevinle38@gmail.com>
2021-06-18 15:32:57 +0700

    shiping card

--- lib/views/dashboard/shipping/shipping_tab.dart
+++ lib/views/dashboard/shipping/shipping_tab.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/cupertino.dart';
+import 'package:hk_clothes/views/dashboard/shipping/widgets/shippingproduct.dart';
 
 class ShippingTab extends StatelessWidget {
   const ShippingTab({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.builder(scrollDirection: Axis.vertical,
+        itemCount: 1,
+        itemBuilder: (BuildContext context, int index) {
+      return ShippingProduct(productID: 'productID', productName: 'productName', productPrice: 'productPrice');
+
+    });
   }
 }
