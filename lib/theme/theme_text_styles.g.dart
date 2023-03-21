02e042f5a1cb40b72b3f3745e6f6907edeefc1a4
Parabeac-Nest <admin@parabeac.com>
2023-03-21 08:32:42 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/theme_text_styles.g.dart
@@ -0,0 +1,70 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class ThemeTextStyles {
+
+static const TextStyle headline1 = TextStyle(
+  fontSize: 96,
+  fontWeight: FontWeight.w300,
+  letterSpacing: -1.5,
+  fontFamily: 'Quicksand',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle headline2 = TextStyle(
+  fontSize: 60,
+  fontWeight: FontWeight.w300,
+  letterSpacing: -0.5,
+  fontFamily: 'Quicksand',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle headline3 = TextStyle(
+  fontSize: 48,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Quicksand',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle headline4 = TextStyle(
+  fontSize: 34,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Quicksand',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle headline5 = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0.18000000715255737,
+  fontFamily: 'Quicksand',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle headline6 = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0.15000000596046448,
+  fontFamily: 'Quicksand',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+}
